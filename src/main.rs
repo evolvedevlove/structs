dbc008bfb01ac67d4be6f06f6923a6d3b011abc3
evolvedevlove <pattyworkshardforthemoney@gmail.com>
2023-12-19 08:19:50 -0700

    added lifts using touple structs

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,9 @@ impl Mountain{
     }
 }
 
+struct Gondola(i64, u64);
+struct Chair(u64, u64);
+
 fn build_skier(cool_name: String, secret_hand_shake: String) -> Skier {
     Skier {
         active: true,
@@ -70,10 +73,40 @@ fn main() {
     println!("ya {}, i wanna shred that {}m tall mountain", skier2.cool_name, sunshine.height);
     skier.days_on_hill += 1;
     skier2.days_on_hill += 1;
-    println!("{} {}", skier.days_on_hill, skier2.days_on_hill);
+    // there is a better way to do the below and i will do it i promise
+    println!("now {} has been on the hill {} and {} has been on the hill {}", skier.cool_name, skier.days_on_hill, skier2.cool_name, skier2.days_on_hill );
 
     println!(
         "The cost per meter ratio for this hill is: {}",
         sunshine.cost_per_meter()
      );
+
+     // 5 - create new mountain and update it to resuse data from generic mountain
+     let lake_louise = Mountain {
+        nick_name: String::from("louie louie"),
+        ..generic_mountain
+     };
+     
+     println!("{}, {}", lake_louise.cost, lake_louise.nick_name);
+
+    // 6 - make a gondola for sunshine 
+    // there must be the origin height is -500 and the base camp is 0
+    let up_to_base = Gondola(-500, 0);
+    println!("sunshine gondola starts {} below the base camp", up_to_base.0);
+
+    // 7 - make a gondola for lake louise where the bottom is 0 and the top is the top of the mountain
+    let to_the_top = Gondola(0, lake_louise.height);
+    println!("louise has a gondola that goes up to the top which is {} high", to_the_top.1);
+    
+    // 8 - make a chair for sunshine that starts at base and goes to the top of the mountain
+    let angel = Chair(0, sunshine.height);
+    println!("sunshine has a chair that goes up to top which is {} tall", angel.1);
+
+    // 9 - make a chair for lake louise that starts at 800 and goes +50 above the peak (the new chair!)
+    let juniper = Chair(800, lake_louise.height + 50);
+    println!("louise has a chair that goes 50 above the top which is {} high total", juniper.1);
+    
+    // now we start to encounter abstraction problems ---
+    // should the mountain have a list of chairs and a list of gondolas on it?
+    // probably - lets do that in the next session    
 }
